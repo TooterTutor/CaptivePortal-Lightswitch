6d8b24273e70144eefa12de694889d11c5f5eb5c
TooterTutor <roguedren@gmail.com>
2022-08-10 17:00:57 -0600

    Added Comments to esp32-captivePortal file

--- esp32-captivePortal.ino
+++ esp32-captivePortal.ino
@@ -1,12 +1,15 @@
+// Adding the neccessary libraries to work properly
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <DNSServer.h>
 #include <AsyncTCP.h>
 #include "ESPAsyncWebServer.h"
 
+// Including the SPI Flash File System libraries for uploading html files to ESP32
 #include "SPIFFS.h"
 #include "FS.h"
 
+// Defining the serial pins to transmit received commands from html to Arduino Uno
 #define RXp2 16
 #define TXp2 17
 
@@ -14,12 +17,13 @@
 DNSServer dnsServer;
 AsyncWebServer server(80);
 
-// Replace with your network credentials
+// Defining network credentials
+// Change before uploading!
 const char *ssid = "ESP32-Access-Point";
-const char *password = "Your-Password-Here";
+const char *password = "Some-Dumb-Old-P4ssword!";
 
 
-
+// Setting up the Directory Listing in Serial Monitor for SPIFFS storage mounting
 void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
    Serial.printf("Listing directory: %s\r\n", dirname);
 
@@ -51,16 +55,18 @@ void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
    }
 }
 
-
+// Defining a Captive Request Handler for Captive Portal
+// Main calls for all files go in this class
 class CaptiveRequestHandler : public AsyncWebHandler {
 public:
   CaptiveRequestHandler() {
-      /* THIS IS WHERE YOU CAN PLACE THE CALLS */
+      // THIS IS WHERE YOU PLACE THE CALLS
+      // Defines all of the main HTML pages before load
       server.on("/", HTTP_GET, [] (AsyncWebServerRequest *request)
       {
         AsyncWebServerResponse* response = request->beginResponse(SPIFFS, "/index.html", "text/html");
         request->send(response);
-        Serial.println("Client Connected");
+        Serial.println("Client Connected and Requested Main Page");
       });
       server.on("/lightswitch.html", HTTP_GET, [] (AsyncWebServerRequest *request)
       {
@@ -74,6 +80,7 @@ public:
         request->send(response);
         Serial.println("Client Requested Whistle Page");
       });
+      // Still being worked on!
       server.on("/stereo.html", HTTP_GET, [] (AsyncWebServerRequest *request)
       {
         AsyncWebServerResponse* response = request->beginResponse(SPIFFS, "/stereo.html", "text/html");
@@ -81,11 +88,13 @@ public:
         Serial.println("Client Requested Stereo Page");
       });
       
+      // Not neccesary, but for extra measures
       server.onNotFound([](AsyncWebServerRequest *request) {
         AsyncWebServerResponse* response = request->beginResponse(SPIFFS, "/notFound.html", "text/html");
         request->send(response);
        });
-
+       
+       // Defining all of the CSS and PNG files
       server.on("/web.css", HTTP_GET, [](AsyncWebServerRequest *request) {
         AsyncWebServerResponse* response = request->beginResponse(SPIFFS, "/web.css", "text/css");
          request->send(response);
@@ -107,7 +116,7 @@ public:
     //request->addInterestingHeader("ANY");
     return true;
   }
-
+  // When someone connects (client), which page should they be given (index.html), and should it be downloaded (false)
   void handleRequest(AsyncWebServerRequest *request) {
     request->send(SPIFFS, "/index.html", String(), false);
   }
@@ -115,6 +124,7 @@ public:
 
 void setupServer()
 { 
+  // When button is pressed on Lightswitch Page, send command through Serial2 to listening Arduino Uno
   server.on("/on", HTTP_GET, [] (AsyncWebServerRequest *request)
   {
       Serial2.write("ON");
@@ -156,7 +166,7 @@ void setupServer()
       Serial2.write("UNLOCK ALL");
   });
 
-  
+  // When button is pressed on Whistle page, send command through Serial2 to listening Arduino Uno
   server.on("/TooterTutor", HTTP_GET, [] (AsyncWebServerRequest *request)
   {
       Serial2.write("TooterTutor");
@@ -173,8 +183,9 @@ void setupServer()
   {
       Serial2.write("StompGoblin");
   });
-
-
+  // Still a work in progress!
+  // When button is pressed on Stereo screen, send command through Serial2
+  // to Arduino Uno to transmit a series of IR information to an IR controlled Stereo System
   server.on("/stereo_power", HTTP_GET, [] (AsyncWebServerRequest *request)
   {
     Serial2.write("stereo_power");
@@ -254,34 +265,37 @@ void setupServer()
 
 void setup()
 {
-  //your other setup stuff...
+  // Start Both Serial (Serial Monitor) and Serial2 (Communication to Arduino Uno)
   Serial.begin(115200);
   Serial2.begin(9600, SERIAL_8N1, RXp2, TXp2);
 //  IrSender.begin(); // Start with IR_SEND_PIN as send pin and if NO_LED_FEEDBACK_CODE is NOT defined, enable feedback LED at default feedback LED pin
 
-  
+  // If SPIFFS has not mounted, throw an error in the Serial Monitor
   if(!SPIFFS.begin(true)){
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
   }
   
-  //List contents of SPIFFS
+  // List contents of SPIFFS
   listDir(SPIFFS, "/", 0);
 
+  // Start Access Point Mode with ssid and password defined earlier and print info
   Serial.println();
   Serial.println("Setting up AP Mode");
   WiFi.mode(WIFI_AP); 
   WiFi.softAP(ssid, password);
-  
+
+  // Print Access Point IP address and then set up Async Webserver
   Serial.print("AP IP address: ");Serial.println(WiFi.softAPIP());
   Serial.println("Setting up Async WebServer");
   setupServer();
 
+  // Start DNS server and add the CaptiveRequestHandler that was defined earlier
   Serial.println("Starting DNS Server");
   dnsServer.start(53, "*", WiFi.softAPIP());
   server.addHandler(new CaptiveRequestHandler()).setFilter(ON_AP_FILTER);//only when requested from AP
 
-  //more handlers...
+  // Start the server
   server.begin();
   Serial.println("All Done!");
 }
